a13119ee0d108ad9b9ed17fa1d5cf27a4043c604
Julio Alberto <julioj1081@hotmail.com>
2020-12-13 19:23:38 -0600

    Ejemplo basico de POO

---
+++ POO/Coche.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    class Coche
+    {
+        private int _id;
+        private string _marca;
+        private string _modelo;
+        private int _km;
+        private double _precio;
+
+        //creacion de constructor
+        public Coche(int id, string marca, string modelo, int km, double precio)
+        {
+            this._id = id;
+            this._marca = marca;
+            this._modelo = modelo;
+            this._km = km;
+            this._precio = precio;
+        }
+
+        //creacion de get y set
+        public int id { get => _id; set => _id = value; }
+        public string Marca { get => _marca; set => _marca = value; }
+        public string Modelo { get => _modelo; set => _modelo = value; }
+        public int Km { get => _km; set => _km = value; }
+        public double Precio { get => _precio; set => _precio = value; }
+
+        //sirve para mostrar la informacion del objeto
+        public override string ToString()
+        {
+            return "Marca"+Marca+", Modelo:"+Modelo + ", Con un precio de "+Precio;
+        }
+    }
+}
---
+++ POO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coche c = new Coche(1, "BMW", "4", 100, 12000);
+            Console.WriteLine(c.id);
+            Console.WriteLine(c.Marca);
+            Console.WriteLine(c.Modelo);
+            Console.WriteLine(c.Km);
+            Console.WriteLine(c.Precio);
+            //agregar nuevo valor 
+            c.Precio = 15000;
+            //return 
+            Console.WriteLine(c.ToString());
+            Console.ReadKey();
+        }
+    }
+}
